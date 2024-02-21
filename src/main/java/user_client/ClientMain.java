8bbf3769c10fd315399cf5cce0c5eeecd941aa28
Daniil Temnyj <temnyy1@gmail.com>
2024-02-21 13:17:24 +0300

    Removed user-admin window polymorph

--- src/main/java/user_client/ClientMain.java
+++ src/main/java/user_client/ClientMain.java
@@ -1,12 +1,12 @@
 package user_client;
 import java.io.IOException;
 
-import user_client.gui.UserWindow;
+import user_client.gui.PatcherWindow;
 import user_client.utils.RunCourgette;
 
 public class ClientMain {
     public static void main(String[] args) throws IOException {
         RunCourgette.unpackCourgette();
-        new UserWindow();
+        new PatcherWindow();
     }
 }
